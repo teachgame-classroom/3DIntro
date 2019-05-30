a75558e843acec14cd76b03d60dd606aae312251
teachgame <teachgame@163.com>
2019-05-30 09:46:01 +0800

    Day04 - Camera Follow Done

---
+++ Assets/_3DIntroLesson/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform followTarget;
+    public float smooth = 5;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - followTarget.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetCamPos = followTarget.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smooth * Time.deltaTime);
+    }
+}
